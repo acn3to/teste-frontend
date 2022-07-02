52450da5d0098deed6f74916106595bc8331df18
Arnaldo Neto <costanetoow@gmail.com>
2022-07-02 19:45:17 -0300

    feat: create layout component

---
+++ src/components/Layout/styles.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-wrap: nowrap;
+  justify-content: space-between;
+  align-items: center;
+  align-content: center;
+  height: 100%;
+`;
